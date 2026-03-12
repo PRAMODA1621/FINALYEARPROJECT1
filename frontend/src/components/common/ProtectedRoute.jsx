0ab1eec5da2c148451d45db25e085010c6dae644
PRAMODA1621 <nppramodaofficial@gmail.com>
2026-03-12 20:44:03 +0530

    fix server port for render

--- frontend/src/components/common/ProtectedRoute.jsx
+++ frontend/src/components/common/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
-import { useAuth } from '../../contexts/Authcontext';
+import { useAuth } from '../../contexts/AuthContext';
 import LoadingSpinner from './LoadingSpinner';
 
 const ProtectedRoute = () => {
