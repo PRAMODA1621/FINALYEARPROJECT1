a66d0e538d6d94ad1520033d64a22c9aa2d803b0
PRAMODA1621 <nppramodaofficial@gmail.com>
2026-03-14 00:53:21 +0530

    added ollama chatbot

--- frontend/src/api/chatbotApi.js
+++ frontend/src/api/chatbotApi.js
@@ -1,19 +1,17 @@
 import axios from "axios";
 
-const CHATBOT_URL = import.meta.env.VITE_CHATBOT_URL;
+const CHATBOT_URL =
+  import.meta.env.VITE_CHATBOT_URL ||
+  "https://finalyearproject1-1.onrender.com";
 
 const chatbotApi = {
-
   sendMessage: async (message) => {
-
     const response = await axios.post(`${CHATBOT_URL}/api/chat`, {
       message
     });
 
     return response.data;
-
   }
-
 };
 
 export default chatbotApi;
